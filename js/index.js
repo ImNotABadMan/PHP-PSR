19c5742ed57ff96c90e7ccb5efeab48a51fc8f6a
binz <bin_zheng@limei-trading.com>
2019-05-30 22:22:51 +0800

    增加内容

--- js/index.js
+++ js/index.js
@@ -31,5 +31,28 @@ let img = new Vue({
 
 function enlarge(obj)
 {
-    alert(obj.text);
+    console.log(obj.$refs.imgSrc.src);
+    $('.mask-layer-img').attr('src', obj.$refs.imgSrc.src);
+    /**
+     * mask-btn 按钮触发
+     * mask-layer 最外层
+     * mask-content 编辑层
+     * */
+    $('.mask-layer').fadeToggle();
+    $('.mask-content').fadeToggle();
 }
+
+$(function () {
+    $('.mask-layer').click(function (event) {
+        if (event.target !== this) {
+            return;
+        }
+        $(this).fadeToggle();
+        $('.mask-content').fadeToggle();
+    });
+
+    $('.mask-content').each(function (index,item) {
+        $(this).css("margin-left", -$(this).innerWidth() * 9.2);
+        $(this).css("margin-top", -$(this).innerHeight()/10);
+    })
+});
