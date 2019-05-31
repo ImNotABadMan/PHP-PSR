667d807a8c9812e0d3e1f5d1c0c4996777818929
binz <bin_zheng@limei-trading.com>
2019-05-31 12:00:35 +0800

    pagination

--- js/index.js
+++ js/index.js
@@ -12,6 +12,10 @@ let swiper = new Swiper('.swiper-container', {
     },
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
     },
 });
 
@@ -23,24 +27,22 @@ let img = new Vue({
     },
     methods: {
         show() {
-            enlarge(this)
+            console.log(this.$refs.imgSrc.src);
+            $('.mask-layer-img').attr('src', this.$refs.imgSrc.src);
+            /**
+             * mask-btn 按钮触发
+             * mask-layer 最外层
+             * mask-content 编辑层
+             * */
+            $('.mask-layer').fadeToggle();
+            $('.mask-content').each(function (index,item) {
+                $(this).css("margin-left", -$(this).innerWidth()/2);
+                $(this).css("margin-top", -$(this).innerHeight()/10);
+            }).fadeToggle();
         }
     },
 });
 
-
-function enlarge(obj)
-{
-    console.log(obj.$refs.imgSrc.src);
-    $('.mask-layer-img').attr('src', obj.$refs.imgSrc.src);
-    /**
-     * mask-btn 按钮触发
-     * mask-layer 最外层
-     * mask-content 编辑层
-     * */
-    $('.mask-layer').fadeToggle();
-    $('.mask-content').fadeToggle();
-}
 
 $(function () {
     $('.mask-layer').click(function (event) {
